a6966a2ec85722bf1e47ca85d981712b7cff27a8
basicxd <mikail-fener@hotmail.com>
2019-04-04 12:55:30 +0200

    Add project files.

---
+++ UDPSensorReceiver/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UDPSensorReceiver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
