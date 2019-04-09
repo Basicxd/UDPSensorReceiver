21b8a6437081079bb9200eb3b3d831d5c249dc5c
basicxd <mikail-fener@hotmail.com>
2019-04-09 12:19:34 +0200

    Miu

--- UDPSensorReceiver/Socket.cs
+++ UDPSensorReceiver/Socket.cs
@@ -11,16 +11,16 @@ namespace UDPSensorReceiver
         public void broadMain()
         {
             //Creates a UdpClient for reading incoming data.
-            UdpClient udpServer = new UdpClient(7000);
+            UdpClient udpServer = new UdpClient(11111);
 
             //Creates an IPEndPoint to record the IP Address and port number of the sender.  
             IPAddress ip = IPAddress.Any;
-            IPEndPoint RemoteIpEndPoint = new IPEndPoint(ip, 7000);
+            IPEndPoint RemoteIpEndPoint = new IPEndPoint(ip, 11111);
 
             try
             {
                 // Blocks until a message is received on this socket from a remote host (a client).
-                Console.WriteLine("Server is blocked");
+                Console.WriteLine("Reciever is started");
                 while (true)
                 {
                     Byte[] receiveBytes = udpServer.Receive(ref RemoteIpEndPoint);
@@ -33,22 +33,39 @@ namespace UDPSensorReceiver
                     string time = data[2];
                     string cO = data[3];
                     string nOx = data[4];
+                    //double cO = Convert.ToDouble(data[3]);
+                    //double nOx = Convert.ToDouble(data[4]);
                     string particleLevel = data[5];
 
-                    Console.WriteLine(sender + location + time + cO + nOx + particleLevel);
+                    string[] some = data[3].Split(':');
+                    string some1 = some[3];
 
-                    Console.WriteLine(receivedData);
-                    //Console.WriteLine("Received from: " + clientName.ToString() + " " + text.ToString());
+                    Console.WriteLine(some1);
 
-                    //string sendData = "Server: " + text.ToUpper();
-                    //Byte[] sendBytes = Encoding.ASCII.GetBytes(sendData);
+                    for (int i = 0; i <= 5; i++)
+                    {
+                        Console.WriteLine(i + "   " + data[i]);
+                    }
 
-                    //udpServer.Send(sendBytes, sendBytes.Length, RemoteIpEndPoint);
 
-                    Console.WriteLine("This message was sent from " +
-                                      RemoteIpEndPoint.Address.ToString() +
-                                      " on their port number " +
-                                      RemoteIpEndPoint.Port.ToString());
+                    //Console.WriteLine($"{sender} {location}");
+
+                    //Console.WriteLine(sender);
+                    //Console.WriteLine(location);
+                    //Console.WriteLine(time);
+                    //Console.WriteLine(cO);
+                    //Console.WriteLine(nOx);
+                    //Console.WriteLine(particleLevel);
+                    //Console.WriteLine();
+
+
+                    //Console.WriteLine(receivedData);
+
+                    //Console.WriteLine("This message was sent from " +
+                    //                  RemoteIpEndPoint.Address.ToString() +
+                    //                  " on their port number " +
+                    //                  RemoteIpEndPoint.Port.ToString());
+
                 }
             }
             catch (Exception e)
